97e4a7c530bec7802eaa6a9e80db69124d59c9b2
chamannarved <chamannarved@gmail.com>
2020-08-16 22:26:15 +0530

    fix typo in app.js

--- 1-color-flipper/setup/app.js
+++ 1-color-flipper/setup/app.js
@@ -5,7 +5,7 @@ const color = document.querySelector(".color");
 
 btn.addEventListener("click", function () {
   // get random number between 0 - 3
-  const rendomNumber = getRandomNumber();
+  const randomNumber = getRandomNumber();
 
   document.body.style.backgroundColor = colors[randomNumber];
   color.textContent = colors[randomNumber];
