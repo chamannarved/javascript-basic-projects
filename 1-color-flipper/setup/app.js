89cc5589a667932843cc5654c3994ced8a1b5848
chamannarved <chamannarved@gmail.com>
2020-08-16 22:18:53 +0530

    get random values in variable

--- 1-color-flipper/setup/app.js
+++ 1-color-flipper/setup/app.js
@@ -3,9 +3,9 @@ const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
-btn.addEventListener("click", function(){
-    // get random number between 0 - 3
-    const rendomNumber;
+btn.addEventListener("click", function () {
+  // get random number between 0 - 3
+  const rendomNumber = getRandomNumber();
 });
 
 function getRandomNumber() {
