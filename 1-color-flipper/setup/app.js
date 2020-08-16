608521a2d59b92605c6b8fc9c138679f9060dcaf
chamannarved <chamannarved@gmail.com>
2020-08-16 22:20:22 +0530

    chande body color and color value on click btn

--- 1-color-flipper/setup/app.js
+++ 1-color-flipper/setup/app.js
@@ -6,6 +6,9 @@ const color = document.querySelector(".color");
 btn.addEventListener("click", function () {
   // get random number between 0 - 3
   const rendomNumber = getRandomNumber();
+
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
 });
 
 function getRandomNumber() {
