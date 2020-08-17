235470b2780047b97dc359be887b447fa9e41092
chamannarved <chamannarved@gmail.com>
2020-08-17 12:11:58 +0530

    check btn clicked and change count value

--- 2-counter/setup/app.js
+++ 2-counter/setup/app.js
@@ -8,5 +8,15 @@ const btns = document.querySelectorAll(".btn");
 btns.forEach(function (btn) {
   btn.addEventListener("click", function (e) {
     const styles = e.currentTarget.classList;
+
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      // reset button
+      count = 0;
+    }
+    value.textContent = count;
   });
 });
