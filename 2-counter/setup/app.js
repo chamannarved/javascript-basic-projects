ae070825a76dae6eb0b808205b1364d52ad4b4ba
chamannarved <chamannarved@gmail.com>
2020-08-17 11:54:22 +0530

    add forEach() for select btns

--- 2-counter/setup/app.js
+++ 2-counter/setup/app.js
@@ -4,3 +4,7 @@ let count = 0;
 // select value and buttons
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+  console.log(btn);
+});
