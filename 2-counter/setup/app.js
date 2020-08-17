80796101932ffd9b517bfba18f0ad9bc7053c6c0
chamannarved <chamannarved@gmail.com>
2020-08-17 11:57:32 +0530

    add event listener for btn click

--- 2-counter/setup/app.js
+++ 2-counter/setup/app.js
@@ -6,5 +6,7 @@ const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
 
 btns.forEach(function (btn) {
-  console.log(btn);
+  btn.addEventListener("click", function (e) {
+    console.log(e.currentTarget);
+  });
 });
