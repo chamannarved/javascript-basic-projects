911966a612d41e721d20bf151baed647ba4cfb13
chamannarved <chamannarved@gmail.com>
2020-08-17 12:00:30 +0530

    store btn class in a variabel

--- 2-counter/setup/app.js
+++ 2-counter/setup/app.js
@@ -7,6 +7,6 @@ const btns = document.querySelectorAll(".btn");
 
 btns.forEach(function (btn) {
   btn.addEventListener("click", function (e) {
-    console.log(e.currentTarget);
+    const styles = e.currentTarget.classList;
   });
 });
